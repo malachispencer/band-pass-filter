35fb8219f559c75abecfcc4b32ca14da7dd6c81c
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-02-01 11:24:30 +0000

    returns the same array when all frequencies are in range

--- src/bandPassFilter.js
+++ src/bandPassFilter.js
@@ -1,5 +1,9 @@
 function bandPassFilter(soundWave, lowThreshold, highThreshold) {
   if (!soundWave.length) { throw 'No sound wave found'; }
+
+  return soundWave.map(frequency => {
+    return frequency;
+  });
 }
 
 module.exports = bandPassFilter;
--- test/bandPassFilter.test.js
+++ test/bandPassFilter.test.js
@@ -4,4 +4,8 @@ describe('bandPassFilter', () => {
   test('throws an error when not passed a frequency', () => {
     expect(() => { bandPassFilter([], 30, 90) }).toThrow('No sound wave found');
   });
+
+  test('returns the same array when all frequencies are in range', () => {
+    expect(bandPassFilter([30, 60, 90], 30, 90)).toEqual([30, 60, 90]);
+  });
 });
