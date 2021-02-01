3246fbb4acef5f348eadcec663adaa088d3d49b7
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-02-01 11:35:47 +0000

    handles sound wave with frequencies above and below thresholds

--- src/bandPassFilter.js
+++ src/bandPassFilter.js
@@ -2,13 +2,9 @@ function bandPassFilter(soundWave, lowThreshold, highThreshold) {
   if (!soundWave.length) { throw 'No sound wave found'; }
 
   return soundWave.map(frequency => {
-    if (frequency < lowThreshold) {
-      return lowThreshold;
-    } else if (frequency > highThreshold) {
-      return highThreshold;
-    } else {
-      return frequency;
-    }
+    return frequency < lowThreshold ? lowThreshold :
+      frequency > highThreshold ? highThreshold :
+        frequency;
   });
 }
 
--- test/bandPassFilter.test.js
+++ test/bandPassFilter.test.js
@@ -9,13 +9,17 @@ describe('bandPassFilter', () => {
     expect(bandPassFilter([30, 60, 90], 30, 90)).toEqual([30, 60, 90]);
   });
 
-  test('values below low threshold become low threshold', () => {
+  test('frequencies below low threshold become low threshold', () => {
     expect(bandPassFilter([30], 40, 90)).toEqual([40]);
     expect(bandPassFilter([30, 60, 90], 70, 100)).toEqual([70, 70, 90]);
   });
 
-  test('values above high threshold become high threshold', () => {
+  test('frequencies above high threshold become high threshold', () => {
     expect(bandPassFilter([90], 30, 80)).toEqual([80]);
     expect(bandPassFilter([30, 60, 90], 30, 50)).toEqual([30, 50, 50]);
+  });
+
+  test('handles sound wave with frequencies above and below thresholds', () => {
+    expect(bandPassFilter([60, 10, 45, 60, 1500], 40, 1000)).toEqual([60, 40, 45, 60, 1000]);
   });
 });
